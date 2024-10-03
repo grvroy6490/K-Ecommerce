d20ce26b19541365d1054bcdd36154bfb65f70a1
unknown <abhimanyudiwaker@gmail.com>
2024-10-04 00:31:44 +0530

    added deals and offers slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,6 +31,47 @@ $(document).ready(() => {
             }
         }
     })
+
+    const dealSlider = new Swiper('.deals-slider',{
+        autoplay: {
+            delay: 3000,
+
+        },
+        allowTouchMove:true,
+        slidesPerView:1,
+        spaceBetween:20,
+        
+        breakpoints:{
+            768:{
+                slidesPerView:3
+            },
+           
+            1920:{
+                slidesPerView:4
+            }
+            
+        }
+    })
+    const offerSlider = new Swiper('.offers-slider', {
+        autoplay: {
+            delay: 3000,
+        },
+        speed: 300,
+        allowTouchMove: true,
+        loop:true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        initialSlide: 2, // Center the third slide initially, index starts from 0
+        
+        breakpoints: {
+            768: {
+                slidesPerView: 1.5,
+                centeredSlides: true,
+                initialSlide: 2, // Keeps the third slide centered at this breakpoint
+            }
+        }
+    });
         
 })
 
