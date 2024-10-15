fb78e76edc70ad5e46a2f40c008090e2ba96436d
unknown <abhimanyudiwaker@gmail.com>
2024-10-16 00:33:53 +0530

    added feature blog and about us

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,6 +52,7 @@ $(document).ready(() => {
             
         }
     })
+
     const offerSlider = new Swiper('.offers-slider', {
         autoplay: {
             delay: 3000,
@@ -69,6 +70,48 @@ $(document).ready(() => {
                 slidesPerView: 1.5,
                 centeredSlides: true,
                 initialSlide: 2, // Keeps the third slide centered at this breakpoint
+            }
+        }
+    });
+
+    const reviewSlider = new Swiper('.review-slider', {
+        autoplay: {
+            delay: 3000,
+        },
+        speed: 300,
+        allowTouchMove: true,
+        loop:true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        spaceBetween: 40,
+        initialSlide: 2, // Center the third slide initially, index starts from 0
+        
+        breakpoints: {
+            768: {
+                slidesPerView: 1.5,
+                centeredSlides: true,
+                initialSlide: 2, // Keeps the third slide centered at this breakpoint
+            }
+        }
+    });
+
+    const featuredBlogSlider = new Swiper('.blog-slider', {
+        autoplay: {
+            delay: 3000,
+        },
+        speed: 300,
+        allowTouchMove: true,
+        loop:true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        spaceBetween: 40,
+        initialSlide: 1, // Center the third slide initially, index starts from 0
+        
+        breakpoints: {
+            768: {
+                slidesPerView: 2.5,
+                centeredSlides: true,
+                initialSlide: 1, // Keeps the third slide centered at this breakpoint
             }
         }
     });
